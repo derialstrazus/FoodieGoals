d91c7ddf1434884282106dac5c44eb376a72bd6a
Derek Lee <derekleekokfui@gmail.com>
2018-06-11 06:45:38 -0500

    moved everything to Azure

---
+++ Data/FoodieGoals.API/Controllers/HealthCheckController.cs
@@ -0,0 +1,42 @@
+﻿using FoodieGoals.Data.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FoodieGoals.Controllers
+{
+    public class HealthCheckController : ApiController
+    {
+
+        //http://api.foodiegoals.local/api/healthcheck
+        public IHttpActionResult Get()
+        {
+            return Ok("Hello");
+        }
+
+        //http://api.foodiegoals.local/api/healthcheck/customroute
+        [HttpGet, Route("api/healthcheck/customroute")]
+        public IHttpActionResult CustomRoute()
+        {
+            return Ok("How did you find me?");
+        }
+
+        //http://api.foodiegoals.local/api/healthcheck/callback?value=5
+        [HttpGet, Route("api/healthcheck/callback")]
+        public IHttpActionResult GetCallback(int value)
+        {
+            return Ok(value);
+        }
+
+        //http://api.foodiegoals.local/api/healthcheck/testdatalayer?val1=2&val2=3
+        [HttpGet, Route("api/healthcheck/testdatalayer")]
+        public IHttpActionResult TestDataLayer(int val1, int val2)
+        {
+            var returnThis = HealthCheckManager.AddThese(val1, val2);
+            return Ok(returnThis);
+        }
+    }
+}
---
+++ Data/FoodieGoals.Data/Managers/HealthCheckManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodieGoals.Data.Managers
+{
+    public class HealthCheckManager
+    {
+
+        public static int AddThese(int val1, int val2)
+        {
+            return val1 + val2;
+        }
+
+    }
+}
